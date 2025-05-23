3b2c496c719ae41589eac6979a38195a97d41050
aldosianturi <aldo23ti@mahasiswa.pcr.ac.id>
2025-05-23 09:49:30 +0700

    Perubahan Teks

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Raiim's Server</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        ALDO E SIANTURI G23 TI C
       </p>
     </>
   )
